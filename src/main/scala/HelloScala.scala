2fa8b756c5c86e026c1c414050439742027f9cd0
Francesco Dente <dente.francesco1996@gmail.com>
2020-07-01 17:51:35 +0200

    del: dummy test case

--- src/main/scala/HelloScala.scala
+++
@@ -1,3 +0,0 @@
-object HelloScala {
-  def sayHello: String = "Hello world!"
-}
--- src/test/scala/HelloScalaTest.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.{FunSuite, Matchers}
-
-class HelloScalaTest extends FunSuite with Matchers {
-
-  test("testSayHello") {
-    HelloScala.sayHello should be ("Hello world!")
-  }
-
-}
