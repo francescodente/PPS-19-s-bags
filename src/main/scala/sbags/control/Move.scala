6a4588d240e44dcc0ff02b923b8cbe0c9586963f
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-04 15:44:19 +0200

    docs: add scaladoc for Move

--- src/main/scala/sbags/control/Move.scala
+++ src/main/scala/sbags/control/Move.scala
@@ -1,5 +1,14 @@
 package sbags.control
 
+/**
+ * An operation that changes the current Game State and that comprises 0 or more modification of the Board State (e.g. castling);
+ * @tparam S Defines the type of the Game State, with [[sbags.control.BoardGameState]] as an upper-bound.
+ */
 trait Move[S <: BoardGameState[_]] {
+  /**
+   * Executes the move on a given Game State.
+   * @param gameState on which the Move will be applied.
+   * @return the new Game State.
+   */
   def execute(gameState: S): S
 }
