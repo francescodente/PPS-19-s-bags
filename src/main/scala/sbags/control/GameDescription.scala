7383450e02901fc1361a0d5adaf3f3baa3b9f2ef
Francesco Dente <dente.francesco1996@gmail.com>
2020-07-04 14:56:47 +0200

    docs: fix scaladoc with fully qualified names

--- src/main/scala/sbags/control/GameDescription.scala
+++ src/main/scala/sbags/control/GameDescription.scala
@@ -19,17 +19,20 @@ trait GameDescription {
 }
 
 /**
- * Extends a [[GameDescription]] forcing the game state to be a subtype of [[BoardGameState]] and adding the constraint of
- * having a [[Board]] as part of the game state.
+ * Extends a [[sbags.control.GameDescription]] forcing the game state to be a subtype of
+ * [[sbags.control.BoardGameState]] and adding the constraint of having a [[sbags.entity.Board]]
+ * as part of the game state.
  */
 trait BoardGameDescription extends GameDescription {
   /**
-   * Defines the type of the board state used by the [[GameState]] type, that must be a subtype of the [[Board]] type.
+   * Defines the type of the board state used by the GameState type, that must be a subtype of the
+   * [[sbags.entity.Board]] type.
    */
   type BoardState <: Board
 
   /**
-   * Defines the type of game state for this game description, that must be a subtype of [[BoardGameState]].
+   * Defines the type of game state for this game description, that must be a subtype of
+   * [[sbags.control.BoardGameState]].
    */
   type GameState <: BoardGameState[BoardState]
 }
