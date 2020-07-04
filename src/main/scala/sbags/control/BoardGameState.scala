7dbafaec02d70c0daecc4c86dcac7f19d876bd8a
Simone <Simone.magnani4@studio.unibo.it>
2020-07-04 12:34:32 +0200

    docs: first ScalaDoc for BoardGameState

--- src/main/scala/sbags/control/BoardGameState.scala
+++ src/main/scala/sbags/control/BoardGameState.scala
@@ -2,9 +2,22 @@ package sbags.control
 
 import sbags.entity.Board
 
+/**
+ * Represents the state of a particular game: it includes the Board State.
+ * @tparam B defines the type of the Board State with [[Board]] as upper-bound.
+ */
 trait BoardGameState[B <: Board] {
+  /**
+   * Returns the actual Board State relative to this Game State.
+   * @return the actual Board State.
+   */
   def boardState: B
 }
 
+/**
+ * Extends [[BoardGameState]] making it instantiable and forcing the BoardState as val.
+ * @param boardState val of actual BoardStare.
+ * @tparam B defines the type of the Board State with [[Board]] as upper-bound
+ */
 class BasicGameState[B <: Board](val boardState: B) extends BoardGameState[B]
 
