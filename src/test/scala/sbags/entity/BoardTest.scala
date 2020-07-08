0dd5729a8551a0f6103ee8331251fe70a4f5f101
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-07-08 11:32:52 +0200

    fix: test

--- src/test/scala/sbags/entity/BoardTest.scala
+++ src/test/scala/sbags/entity/BoardTest.scala
@@ -5,22 +5,24 @@ import org.scalatest.{FlatSpec, Matchers}
 class BoardTest extends FlatSpec with Matchers {
 
   val tilePosition: Int = 0
+  def newSimpleBoard: BasicBoard {
+    type Tile = Int
+    type Pawn = String
+  } = new BasicBoard {
+    type Tile = Int
+    type Pawn = String
+    override def tiles: Seq[Int] = List(tilePosition)
+  }
 
   behavior of "A board"
 
   it should "return None from not initialize tile" in {
-    val board = new BasicBoard {
-      type Tile = Int
-      type Pawn = String
-    }
+    val board = newSimpleBoard
     board(tilePosition) should be (None)
   }
 
   it should "be able to add a pawn in a tile" in {
-    val board = new BasicBoard {
-      type Tile = Int
-      type Pawn = String
-    }
+    val board = newSimpleBoard
     val pawnName: String = "pawnName"
     board << (pawnName -> tilePosition)
     board(tilePosition) should be (Some(pawnName))
@@ -28,29 +30,20 @@ class BoardTest extends FlatSpec with Matchers {
 
   it should "throw an IllegalStateException when placing a pawn on a non-empty tile" in {
     assertThrows[IllegalStateException] {
-      val board = new BasicBoard {
-        type Tile = Int
-        type Pawn = String
-      }
+      val board = newSimpleBoard
       board << ("pawnName1" -> tilePosition)
       board << ("pawnName2" -> tilePosition)
     }
   }
 
   it should "be able to remove a pawn from a tile" in {
-    val board = new BasicBoard {
-      type Tile = Int
-      type Pawn = String
-    }
+    val board = newSimpleBoard
     board << ("pawnName" -> tilePosition) <# tilePosition
     board(tilePosition) should be (None)
   }
 
   it should "be possible to get a boardMap" in {
-    val board = new BasicBoard {
-      type Tile = Int
-      type Pawn = String
-    }
+    val board = newSimpleBoard
     val pawnValue = "pawnName"
     board << (pawnValue -> tilePosition)
     board.boardMap should contain (tilePosition -> pawnValue)
--- src/test/scala/sbags/entity/GameStateTest.scala
+++ src/test/scala/sbags/entity/GameStateTest.scala
@@ -5,13 +5,19 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class GameStateTest extends FlatSpec with MockFactory with Matchers {
 
-  private val board = new BasicBoard {
+  private val board: BasicBoard = new BasicBoard {
     override type Tile = Int
     override type Pawn = String
+
+    /**
+     *
+     * @return the sequence containing all the valid tile for this board.
+     */
+    override def tiles: Seq[Int] = List()
   }
 
   "A new game state" should "have the board it is passed" in {
-    val gameState = new BasicGameState[BasicBoard](board){
+    val gameState: BasicGameState[BasicBoard] = new BasicGameState[BasicBoard](board){
       type Move = Any
       override def executeMove(move: Any): Unit = {}
     }
