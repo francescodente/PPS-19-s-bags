8405b16111feb77ed35ab08dc52528b8ff247943
Simone <Simone.magnani4@studio.unibo.it>
2020-07-08 13:19:22 +0200

    test: specify behaviour of TurnsStream

--- src/test/scala/sbags/entity/GameStateMixinTest.scala
+++ src/test/scala/sbags/entity/GameStateMixinTest.scala
@@ -17,7 +17,6 @@ class GameStateMixinTest extends FlatSpec with Matchers{
 
   val gameStateTurnsTest = new BasicGameState[BoardTypeTest](board) with Turns[BoardTypeTest, Int] {
     override type Move = Any
-
     override def executeMove(move: Any): Unit = {}
 
     var turn: Option[Int] = Some(0)
@@ -31,6 +30,43 @@ class GameStateMixinTest extends FlatSpec with Matchers{
   it should "be at turn 1 after 1 turn" in {
     gameStateTurnsTest.nextTurn()
     gameStateTurnsTest.turn should be(Some(1))
+  }
+
+  behavior of "A gameState with TurnsStream"
+
+  it should "has turn equal the head of Stream when created" in {
+    val streamTest = Stream(0,1)
+    val gameStateTurnsIteratorTest = new BasicGameState[BoardTypeTest](board) with TurnsStream[BoardTypeTest, Int] {
+      override type Move = Any
+      override def executeMove(move: Any): Unit = {}
+
+      override var remainingTurns: Stream[Int] = streamTest
+    }
+    gameStateTurnsIteratorTest.turn should be(Some(streamTest.head))
+  }
+
+  it should "has turn equal the second value of Stream when created" in {
+    val streamTest = Stream(0,1)
+    val gameStateTurnsIteratorTest = new BasicGameState[BoardTypeTest](board) with TurnsStream[BoardTypeTest, Int] {
+      override type Move = Any
+      override def executeMove(move: Any): Unit = {}
+
+      override var remainingTurns: Stream[Int] = streamTest
+    }
+    gameStateTurnsIteratorTest.nextTurn()
+    gameStateTurnsIteratorTest.turn should be(Some(streamTest(1)))
+  }
+
+  it should "be None when match finishes (no more turn left)" in {
+    val streamTest = Stream(0)
+    val gameStateTurnsIteratorTest = new BasicGameState[BoardTypeTest](board) with TurnsStream[BoardTypeTest, Int] {
+      override type Move = Any
+      override def executeMove(move: Any): Unit = {}
+
+      override var remainingTurns: Stream[Int] = streamTest
+    }
+    gameStateTurnsIteratorTest.nextTurn()
+    gameStateTurnsIteratorTest.turn should be(None)
   }
 
   behavior of "A gameState with GameEndConditions"
