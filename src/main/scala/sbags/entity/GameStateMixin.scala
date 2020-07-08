82d05d0cbaf01b8d416bbdd9cc64130d3955a0c5
Simone <Simone.magnani4@studio.unibo.it>
2020-07-08 14:13:04 +0200

    feat: add TurnsStream trait

--- src/main/scala/sbags/entity/GameStateMixin.scala
+++ src/main/scala/sbags/entity/GameStateMixin.scala
@@ -2,8 +2,16 @@ package sbags.entity
 
 trait Turns[B <: Board, T] { self:BoardGameState[B] =>
   def turn: Option[T]
-
   def nextTurn():Unit
+}
+
+trait TurnsStream[B <: Board, T] extends Turns[B, T] { self:BoardGameState[B] =>
+  override def turn:Option[T] = remainingTurns.headOption
+  protected var remainingTurns: Stream[T]
+
+  override def nextTurn(): Unit = remainingTurns match {
+    case _ #:: t => remainingTurns = t
+  }
 }
 
 trait GameEndCondition[B <: Board, R] { self:BoardGameState[B] =>
