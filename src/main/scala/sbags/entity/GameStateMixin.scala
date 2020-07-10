79f79d76a6f7c8af369025707bff0ea1260b5af1
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-10 11:06:59 +0200

    refactor: move GameEndCondition in a new file

--- src/main/scala/sbags/entity/GameStateMixin.scala
+++ src/main/scala/sbags/entity/GameStateMixin.scala
@@ -71,20 +71,6 @@ trait TwoPlayersAlternateTurn[P] extends Turns with Players[P] {
 }
 
 /**
- * Defines a concept that decides, after making a Move, if the game should end and in that case what the result should be.
- * @tparam R the type of the Result of the game.
- */
-trait GameEndCondition[R] extends GameState {
-  /**
-   * Returns an Optional containing a Result if any, None otherwise.
-   * @return an Optional containing a Result if any, None otherwise.
-   */
-  def gameResult: Option[R]
-  override def executeMove(move: Move): Boolean =
-    gameResult.isEmpty && super.executeMove(move)
-}
-
-/**
  * Represents a game specification where each move ends the current turn.
  * This trait extends [[sbags.entity.Turns]].
  */
