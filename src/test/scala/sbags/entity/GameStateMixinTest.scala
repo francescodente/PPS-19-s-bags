2f008912f9351deb53b3650120a34e9aee80c280
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-10 11:44:33 +0200

    refactor: simplify test

--- src/test/scala/sbags/entity/GameStateMixinTest.scala
+++ src/test/scala/sbags/entity/GameStateMixinTest.scala
@@ -1,76 +1,47 @@
 package sbags.entity
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 
-class GameStateMixinTest extends FlatSpec with Matchers{
-  type BoardTypeTest = BasicBoard {
-    type Tile = Int
-    type Pawn = String
-  }
+class GameStateMixinTest extends FlatSpec with Matchers with MockFactory {
 
-  type TurnState = TestState with Turns[Int]
-
-  private val board: BoardTypeTest = new BasicBoard {
-    type Tile = Int
-    type Pawn = String
-    override def tiles: Seq[Int] = Seq.empty
-  }
-
-  class TestState(val b: BoardTypeTest) extends BasicGameState[BoardTypeTest](b) {
+  class TestState extends GameState {
     override type Move = Any
-    override def executeMove(move: Any): Boolean = {true}
-
+    override def executeMove(move: Any): Boolean = true
     override def ruleSet: RuleSet[Any, this.type] = ???
-  }
-
-  behavior of "A gameState with Turns as Int"
-
-  val gameStateTurnsTest: TurnState = new TestState(board) with Turns[Int] {
-    var turn: Option[Int] = Some(0)
-    override def nextTurn(): Unit = turn = Some(turn.get+1)
-  }
-
-  it should "be at turn 0 when created" in {
-    gameStateTurnsTest.turn should be(Some(0))
-  }
-
-  it should "be at turn 1 after 1 turn" in {
-    gameStateTurnsTest.nextTurn()
-    gameStateTurnsTest.turn should be(Some(1))
   }
 
   behavior of "A gameState with TurnsStream"
 
   it should "has turn equal the head of Stream when created" in {
     val streamTest = Stream(0,1)
-    val gameStateTurnsIteratorTest: TurnState = new TestState(board) with TurnsStream[Int] {
+    val gameStateTurnsIteratorTest = stateWithTurnStream(streamTest)
+    gameStateTurnsIteratorTest.turn should be(Some(streamTest.head))
+  }
+
+  private def stateWithTurnStream(streamTest: Stream[Int]) = {
+    new TestState with TurnsStream[Int] {
       override var remainingTurns: Stream[Int] = streamTest
     }
-    gameStateTurnsIteratorTest.turn should be(Some(streamTest.head))
   }
 
   it should "has turn equal the second value of Stream when created" in {
     val streamTest = Stream(0,1)
-    val gameStateTurnsIteratorTest: TurnState = new TestState(board) with TurnsStream[Int] {
-      override var remainingTurns: Stream[Int] = streamTest
-    }
+    val gameStateTurnsIteratorTest = stateWithTurnStream(streamTest)
     gameStateTurnsIteratorTest.nextTurn()
     gameStateTurnsIteratorTest.turn should be(Some(streamTest(1)))
   }
 
   it should "be None when match finishes (no more turn left)" in {
     val streamTest = Stream(0)
-    val gameStateTurnsIteratorTest: TurnState = new TestState(board) with TurnsStream[Int] {
-      override var remainingTurns: Stream[Int] = streamTest
-    }
+    val gameStateTurnsIteratorTest = stateWithTurnStream(streamTest)
     gameStateTurnsIteratorTest.nextTurn()
     gameStateTurnsIteratorTest.turn should be(None)
   }
 
   behavior of "A gameState with GameEndConditions"
   val MAX_TURN = 2
-  val gameStateTest: TurnState with GameEndCondition[Boolean]
-  = new TestState(board) with Turns[Int] with GameEndCondition[Boolean] {
+  private val gameStateTest = new TestState with Turns[Int] with GameEndCondition[Boolean] {
     var turn: Option[Int] = Some(0)
     override def nextTurn(): Unit = turn = Some(turn.get+1)
     override def gameResult: Option[Boolean] = Some(true) filter (_ => turn.get > MAX_TURN)
