658d79098bab80f86a11f5015baa4bce32564ffc
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-07-10 11:40:18 +0200

    docs: add scalaDoc

--- src/main/scala/examples/tictactoe/TicTacToe.scala
+++ src/main/scala/examples/tictactoe/TicTacToe.scala
@@ -2,18 +2,74 @@ package examples.tictactoe
 
 import sbags.entity._
 
-object TicTacToe extends GameDescription {
-  type BoardState = TicTacToeBoard
-  type GameState = TicTacToeState
+/**
+ * TicTacToePawn represents TicTacToe's pawns.
+ */
+sealed trait TicTacToePawn
 
-  override def newGame: GameState = new TicTacToeState(new TicTacToeBoard)
+/**
+ * Represents the X in a TicTacToe game.
+ */
+case object X extends TicTacToePawn
+
+/**
+ * Represents the O in a TicTacToe game.
+ */
+case object O extends TicTacToePawn
+
+/**
+ * TicTacToeResult represents all the possibles TicTacToc end game result.
+ */
+sealed trait TicTacToeResult
+
+/**
+ * Represents a draw in a TicTacToe's game.
+ */
+case object Draw extends TicTacToeResult
+
+/**
+ * Represents the winner of TicTacToe game.
+ *
+ * @param pawn the type of pawn that wins the game.
+ */
+case class Winner(pawn: TicTacToePawn) extends TicTacToeResult
+
+/**
+ * Represents the type of moves available in a game.
+ */
+sealed trait TicTacToeMove
+
+/**
+ * A move that put X or O in the given position, based on the rules of the game and the current turn.
+ *
+ * @param tile the tile that represents the position on the game board.
+ */
+case class Put(tile: (Int, Int)) extends TicTacToeMove
+
+/**
+ * Represents the TicTacToe board,
+ * with size 3x3
+ * and Pawn of type [[examples.tictactoe.TicTacToePawn]].
+ */
+class TicTacToeBoard extends BasicRectangularBoard(3, 3) {
+  type Pawn = TicTacToePawn
 }
 
+/**
+ * Represents the state of a TicTacToe game,
+ * declare the Move type
+ * support two alternate Players,
+ * end the turn after every Move,
+ * declare the game end conditions.
+ *
+ * @param board the board of the game.
+ */
 class TicTacToeState(board: TicTacToeBoard)
   extends BasicGameState[TicTacToeBoard](board)
     with TwoPlayersAlternateTurn[TicTacToePawn]
     with EndTurnAfterEachMove
     with GameEndCondition[TicTacToeResult] {
+
   type Move = TicTacToeMove
 
   val tuplePlayer: (TicTacToePawn, TicTacToePawn) = (X, O)
@@ -32,14 +88,17 @@ class TicTacToeState(board: TicTacToeBoard)
   override def gameResult: Option[TicTacToeResult] = {
     val result = boardState.tiles.filter(boardState(_).isDefined)
       .groupBy(t => boardState(t).get).find {
-        case (X, l) if checkTris(l) => true
-        case (O, l) if checkTris(l) => true
-        case _ => false
-      }
+      case (X, l) if checkTris(l) => true
+      case (O, l) if checkTris(l) => true
+      case _ => false
+    }
     result map (t => Winner(t._1))
   }
 }
 
+/**
+ * Represents the rules of a TicTacToe game.
+ */
 class TicTacToeRuleSet extends RuleSet[TicTacToeMove, TicTacToeState] {
   override def availableMoves(implicit state: TicTacToeState): Seq[TicTacToeMove] =
     for (t <- state.boardState.tiles; if state.boardState(t).isEmpty) yield Put(t)
@@ -50,17 +109,12 @@ class TicTacToeRuleSet extends RuleSet[TicTacToeMove, TicTacToeState] {
   }
 }
 
-class TicTacToeBoard extends BasicRectangularBoard(3, 3) {
-  type Pawn = TicTacToePawn
+/**
+ * Describe how to create a new TicTacToe game.
+ */
+object TicTacToe extends GameDescription {
+  type BoardState = TicTacToeBoard
+  type GameState = TicTacToeState
+
+  override def newGame: GameState = new TicTacToeState(new TicTacToeBoard)
 }
-
-sealed trait TicTacToeResult
-case object Draw extends TicTacToeResult
-case class Winner(pawn: TicTacToePawn) extends TicTacToeResult
-
-sealed trait TicTacToePawn
-case object X extends TicTacToePawn
-case object O extends TicTacToePawn
-
-sealed trait TicTacToeMove
-case class Put(tile: (Int, Int)) extends TicTacToeMove
