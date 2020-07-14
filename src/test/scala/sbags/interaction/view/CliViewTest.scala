c5411531c2135d5c57533db17b8c1370bec37c07
Simone <Simone.magnani4@studio.unibo.it>
2020-07-14 12:16:12 +0200

    refactor: update name of tests

--- src/test/scala/sbags/interaction/view/CliViewTest.scala
+++ src/test/scala/sbags/interaction/view/CliViewTest.scala
@@ -15,21 +15,21 @@ class CliViewTest extends FlatSpec with MockFactory with Matchers with PrivateMe
    */
 
 
-  it should "be able to display a 1 row board" in {
+  it should "be able to display an empty 1 row board" in {
     val BoardAsString = "1 _ _ _ _ _ \n  1 2 3 4 5 \n"
     val oneRowBoard = new BasicRectangularBoard(5,1)
     val buildBoard = PrivateMethod[String]('buildBoard)
     new CliView[RectangularBoard](i => i+1+"", j=>j+1+"") invokePrivate buildBoard(oneRowBoard) should be(BoardAsString)
   }
 
-  it should "be able to display a 1 column board" in {
+  it should "be able to display an empty 1 column board" in {
     val BoardAsString = "1 _ \n2 _ \n3 _ \n  1 \n"
     val oneColumnBoard = new BasicRectangularBoard(1,3)
     val buildBoard = PrivateMethod[String]('buildBoard)
     new CliView[RectangularBoard](i => i+1+"", j=>j+1+"") invokePrivate buildBoard(oneColumnBoard) should be(BoardAsString)
   }
 
-  it should "be able to display a 2x3 board" in {
+  it should "be able to display an empty 2x3 board" in {
     val BoardAsString = "1 _ _ \n2 _ _ \n3 _ _ \n  1 2 \n"
     val rectangularBoard = new BasicRectangularBoard(2,3)
     val buildBoard = PrivateMethod[String]('buildBoard)
