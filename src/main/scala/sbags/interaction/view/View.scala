ecfeccdff2830233d79eeb23392627d2e6363a3b
Simone <Simone.magnani4@studio.unibo.it>
2020-07-14 13:26:57 +0200

    fix: update i,j => x,y to solve indices bug

--- src/main/scala/sbags/interaction/view/View.scala
+++ src/main/scala/sbags/interaction/view/View.scala
@@ -10,7 +10,7 @@ trait View[G <: GameState] {
 class CliView[B <: RectangularBoard](iModifier: Int => String = i => i+1+"", jModifier: Int => String = j => j+1+"")
   extends View[BoardGameState[B]] {
 
-  def tileToString(board: B)(i: Int)(j:Int): String = board(i,j) match {
+  def tileToString(board: B)(x: Int)(y:Int): String = board(x,y) match {
     case None => "_"
     case Some(pawn) => pawn.toString
   }
@@ -20,11 +20,11 @@ class CliView[B <: RectangularBoard](iModifier: Int => String = i => i+1+"", jMo
     val lf = "\n"
     def buildRow(startingValue: String, cellValue: Int => String, finalValue: String): String = {
       startingValue +
-        (0 until boardState.width).map(j => cellValue(j)).mkString(separator, separator, separator) +
+        (0 until boardState.width).map(x => cellValue(x)).mkString(separator, separator, separator) +
           finalValue
     }
-    (0 until boardState.height).map(i =>
-      buildRow(iModifier(i), tileToString(boardState)(i), lf)
+    (0 until boardState.height).map(y =>
+      buildRow(iModifier(y), tileToString(boardState)(_)(y), lf)
     ).mkString("") + buildRow(separator, jModifier, lf)
   }
 
