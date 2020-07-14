f25df4a7886af8f8f0b06c5a9d375e8248bc2c0d
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-14 12:38:39 +0200

    fix: remove duplicated trait

--- src/main/scala/sbags/interaction/controller/Event.scala
+++ src/main/scala/sbags/interaction/controller/Event.scala
@@ -1,6 +1,5 @@
 package sbags.interaction.controller
 
-trait Event
 case class TileSelected(x: Int, y: Int) extends Event
 case class PawnSelected(pawnName: String) extends Event
 case object Done extends Event
