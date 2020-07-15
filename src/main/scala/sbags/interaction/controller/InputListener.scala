edc1d2c89dcaa6868a1987a2da8d6624787b7106
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-15 12:25:41 +0200

    feat: provide basic InputListener implementation

--- src/main/scala/sbags/interaction/controller/InputListener.scala
+++ src/main/scala/sbags/interaction/controller/InputListener.scala
@@ -5,3 +5,16 @@ trait InputListener {
 }
 
 trait Event
+
+class SequentialInputListener[M](eventsToMove: List[Event] => Option[M]) extends InputListener {
+  private var events: List[Event] = List()
+
+  override def notify(event: Event): Unit = event match {
+    case Done => {
+      val move = eventsToMove(events)
+      //TODO: execute move
+      events = List.empty
+    }
+    case _ => events = event :: events
+  }
+}
