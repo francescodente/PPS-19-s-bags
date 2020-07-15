ec0126cf5d086fe2cb3c74e4a9a24131726bf3e6
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-15 10:12:38 +0200

    test: EventParser on a map with an entry correctly getting the Event

--- src/test/scala/sbags/interaction/controller/EventParserTest.scala
+++ src/test/scala/sbags/interaction/controller/EventParserTest.scala
@@ -2,15 +2,28 @@ package sbags.interaction.controller
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.matching.Regex
+
 class EventParserTest extends FlatSpec with Matchers {
 
   behavior of "An event parser"
 
   val invalidCommand = "invalidCommand"
 
+  val numberString = "123"
+  val numbersRegex: Regex = "[0-9]+".r
+  case class NumberEvent(number: String) extends Event
+  def numberEvent: String => Event = NumberEvent(_)
+
   it should "not find any action if the input is not recognized" in {
     val parser = new EventParser(Map.empty)
     parser.parse(invalidCommand) should be (None)
+  }
+
+  it should "provide the correct event when a string matches" in {
+    val map: Map[Regex, String => Event] = Map(numbersRegex -> numberEvent)
+    val parser = new EventParser(map)
+    parser.parse(numberString) should be (Some(NumberEvent(numberString)))
   }
 
 }
