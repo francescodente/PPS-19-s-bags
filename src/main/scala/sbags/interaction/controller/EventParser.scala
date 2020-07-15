08a46933e87725a019767405cd2d935b32682206
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-15 09:56:43 +0200

    feat: implement EventParser

--- src/main/scala/sbags/interaction/controller/EventParser.scala
+++ src/main/scala/sbags/interaction/controller/EventParser.scala
@@ -2,8 +2,10 @@ package sbags.interaction.controller
 
 import scala.util.matching.Regex
 
-class EventParser(map: Map[Regex, Event]) {
+class EventParser(map: Map[Regex, String => Event]) {
 
-  def parse(command: String): Option[Event] = ???
+  def parse(command: String): Option[Event] = map
+    .filterKeys(_.pattern.asMatchPredicate().test(command))
+    .headOption.map(_._2(command))
 
 }
