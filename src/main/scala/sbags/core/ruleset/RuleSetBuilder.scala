59ce51430410fa4aa2c63b5f392c7ccfdc606b8e
Francesco Dente <dente.francesco1996@gmail.com>
2020-07-16 13:37:41 +0200

    feat: begin rule set DSL implementation

--- src/main/scala/sbags/core/ruleset/RuleSetBuilder.scala
+++ src/main/scala/sbags/core/ruleset/RuleSetBuilder.scala
@@ -1,16 +1,16 @@
 package sbags.core.ruleset
 
-trait RuleSetBuilder[M, G] extends RuleSet[M, G] {
+class RuleSetBuilder[M, G] extends RuleSet[M, G] {
   private var movesGen: List[G => Seq[M]] = List()
-  private var movesExe: List[PartialFunction[(M, G), G]] = List()
+  private var movesExe: List[PartialFunction[M, G => G]] = List()
 
-  def addMoveGen(gen: G => Seq[M]): Unit = movesGen = gen :: movesGen
+  def addMoveGen(gen: G => Seq[M]): Unit = movesGen = movesGen :+ gen
 
-  def addMoveExe(moveExe: PartialFunction[(M, G), G]): Unit = movesExe = moveExe :: movesExe
+  def addMoveExe(moveExe: PartialFunction[M, G => G]): Unit = movesExe = movesExe :+ moveExe
 
   override def availableMoves(state: G): Seq[M] =
     for (gen <- movesGen; move <- gen(state)) yield move
 
   override def executeMove(move: M)(state: G): G =
-    movesExe reduce (_ orElse _) apply(move, state)
+    movesExe.reduce(_ orElse _)(move)(state)
 }
