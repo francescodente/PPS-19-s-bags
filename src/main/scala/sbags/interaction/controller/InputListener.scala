0514d124ad4aeeb545bf4347b0a096bc932e7e64
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-16 17:31:31 +0200

    docs: InputListener scaladoc

--- src/main/scala/sbags/interaction/controller/InputListener.scala
+++ src/main/scala/sbags/interaction/controller/InputListener.scala
@@ -2,12 +2,31 @@ package sbags.interaction.controller
 
 import sbags.core.{Game, GameState}
 
+/**
+ * Gets notified by the view when a new user interaction happened.
+ */
 trait InputListener {
+  /**
+   * Handles the [[sbags.interaction.controller.Event]] emitted by the user interface.
+   * @param event the [[sbags.interaction.controller.Event]] emitted by the user interface.
+   */
   def notify(event: Event)
 }
 
+/**
+ * An event to be emitted by a user interface, representing a user's interaction.
+ * Some examples are selecting a tile, selecting a pawn, ecc.
+ * A particularly important type of event is [[sbags.interaction.controller.Done]], which terminates a sequence of events and executes a move if a match is found.
+ */
 trait Event
 
+/**
+ * An InputListener that requires events to be sent in a specific order, and requires a strategy for translating sequences of events into moves.
+ * @param game the [[sbags.core.Game]] to be handled.
+ * @param eventsToMove a function that transforms a List of [[sbags.interaction.controller.Event]]s into a Move if any, None otherwise.
+ * @tparam State the [[sbags.core.GameState]] type.
+ * @tparam Move the type of the moves in the game.
+ */
 class SequentialInputListener[State <: GameState, Move](game: Game[State, Move],
                                                         eventsToMove: List[Event] => Option[Move]
                                                        ) extends InputListener {
@@ -15,6 +34,12 @@ class SequentialInputListener[State <: GameState, Move](game: Game[State, Move],
   private val gameController = new BasicGameController(game)
   private var events: List[Event] = List()
 
+  /**
+   * For all events that are not [[sbags.interaction.controller.Done]] it just keeps track of them.
+   * If the [[sbags.interaction.controller.Done]] event is received, if a valid move exists, it is executed, otherwise it notifies the view that the move was rejected.
+   * Finally, the list gets emptied.
+   * @param event the [[sbags.interaction.controller.Event]] emitted by the user interface.
+   */
   override def notify(event: Event): Unit = event match {
     case Done =>
       eventsToMove(events).foreach(m => {
