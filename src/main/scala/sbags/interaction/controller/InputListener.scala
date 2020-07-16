8b736c58b0ed5647e4ba061ab5218c201151738b
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-16 11:48:20 +0200

    feat: implement SequentialInputListener

--- src/main/scala/sbags/interaction/controller/InputListener.scala
+++ src/main/scala/sbags/interaction/controller/InputListener.scala
@@ -1,20 +1,21 @@
 package sbags.interaction.controller
 
+import sbags.core.{Game, GameDescription, GameState}
+
 trait InputListener {
   def notify(event: Event)
 }
 
 trait Event
 
-class SequentialInputListener[M](eventsToMove: List[Event] => Option[M]) extends InputListener {
+class SequentialInputListener[State <: GameState, Move](game: Game[State, Move], eventsToMove: List[Event] => Option[Move]) extends InputListener {
+
   private var events: List[Event] = List()
 
   override def notify(event: Event): Unit = event match {
-    case Done => {
-      val move = eventsToMove(events)
-      //TODO: execute move
+    case Done =>
+      eventsToMove(events).foreach(game executeMove _)
       events = List.empty
-    }
     case _ => events = event :: events
   }
 }
