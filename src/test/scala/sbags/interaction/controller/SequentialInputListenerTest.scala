b3ca37a185e8809285b6986df7debe8a2390ca7b
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-16 12:00:53 +0200

    test: correctness of events to move translation

--- src/test/scala/sbags/interaction/controller/SequentialInputListenerTest.scala
+++ src/test/scala/sbags/interaction/controller/SequentialInputListenerTest.scala
@@ -1,6 +1,6 @@
 package sbags.interaction.controller
 
-import examples.tictactoe.{Put, TicTacToe, X}
+import examples.tictactoe.{O, Put, TicTacToe, X}
 import org.scalatest.{FlatSpec, Matchers}
 
 class SequentialInputListenerTest extends FlatSpec with Matchers {
@@ -20,6 +20,40 @@ class SequentialInputListenerTest extends FlatSpec with Matchers {
     inputListener notify Done
 
     game.currentState.board(1,1) should be (Some(X))
+  }
+
+  it should "not perform any move if a sequence of events isn't terminated" in {
+    val game = TicTacToe.newGame
+    val inputListener = new SequentialInputListener[TicTacToe.State, TicTacToe.Move](game, ticTacToeMoves)
+    val initialBoardState = game.currentState.board
+
+    inputListener notify TileSelected(1,1)
+
+    game.currentState.board should be (initialBoardState)
+  }
+
+  it should "not perform any move if two moves in a row are submitted without a Done in between them" in {
+    val game = TicTacToe.newGame
+    val inputListener = new SequentialInputListener[TicTacToe.State, TicTacToe.Move](game, ticTacToeMoves)
+    val initialBoardState = game.currentState.board
+
+    inputListener notify TileSelected(1,1)
+    inputListener notify TileSelected(1,2)
+    inputListener notify Done
+
+    game.currentState.board should be (initialBoardState)
+  }
+
+  it should "be able to perform multiple moves correctly" in {
+    val game = TicTacToe.newGame
+    val inputListener = new SequentialInputListener[TicTacToe.State, TicTacToe.Move](game, ticTacToeMoves)
+
+    inputListener notify TileSelected(1,1)
+    inputListener notify Done
+    inputListener notify TileSelected(1,2)
+    inputListener notify Done
+
+    game.currentState.board(1,2) should be (Some(O))
   }
 
 }
