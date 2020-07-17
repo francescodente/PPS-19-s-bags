307f0bb9f7cffe775a35b9073a034e592c099054
Simone <Simone.magnani4@studio.unibo.it>
2020-07-17 11:41:57 +0200

    refactor: move Event trait to his class

--- src/main/scala/sbags/interaction/controller/Event.scala
+++ src/main/scala/sbags/interaction/controller/Event.scala
@@ -1,5 +1,6 @@
 package sbags.interaction.controller
 
+trait Event
 case class TileSelected(x: Int, y: Int) extends Event
 case class PawnSelected(pawnName: String) extends Event
 case object Done extends Event
