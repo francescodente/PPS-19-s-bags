c6d4e019f9e41823ba874b0037b7f9d73cde418d
Simone <Simone.magnani4@studio.unibo.it>
2020-07-17 12:10:31 +0200

    refactor: solve merge conflict

--- src/main/scala/sbags/interaction/controller/Event.scala
+++ src/main/scala/sbags/interaction/controller/Event.scala
@@ -1,5 +1,10 @@
 package sbags.interaction.controller
 
+/**
+ * An event to be emitted by a user interface, representing a user's interaction.
+ * Some examples are selecting a tile, selecting a pawn, ecc.
+ * A particularly important type of event is [[sbags.interaction.controller.Done]], which terminates a sequence of events and executes a move if a match is found.
+ */
 trait Event
 case class TileSelected(x: Int, y: Int) extends Event
 case class PawnSelected(pawnName: String) extends Event
