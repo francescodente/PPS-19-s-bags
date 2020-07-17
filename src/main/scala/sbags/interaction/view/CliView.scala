2a6bd27bbcc8351e1e260ce532ecadc241a4a9d0
Simone <Simone.magnani4@studio.unibo.it>
2020-07-17 11:42:59 +0200

    refactor: view is generic

--- src/main/scala/sbags/interaction/view/CliView.scala
+++ src/main/scala/sbags/interaction/view/CliView.scala
@@ -2,20 +2,20 @@ package sbags.interaction.view
 
 import sbags.core.{BoardGameState, RectangularBoardStructure}
 
-class CliView(xModifier: Int => String, yModifier: Int => String) extends BasicView {
+class CliView[S <: BoardGameState[B], B <: RectangularBoardStructure](xModifier: Int => String, yModifier: Int => String) extends BasicView[S] {
 
-  type Board <: RectangularBoardStructure
-  type State <: BoardGameState[Board]
-  private val stringifier = Stringifier[State, Board](xModifier, yModifier)
+  private val stringifier = Stringifier[S, B](xModifier, yModifier)
 
-  override def moveAccepted(gameState: State): Unit =
+  override def moveAccepted(gameState: S): Unit =
     println(stringifier.buildBoard(gameState.boardState))
 
   override def moveRejected(): Unit = println("last move was illegal")
 }
 
 object CliView {
-  def apply(xyModifier: Int => String = _ + 1 + ""): CliView = apply(xyModifier, xyModifier)
+  def apply[S <: BoardGameState[B], B <: RectangularBoardStructure]
+      (xyModifier: Int => String = _ + 1 + ""): CliView[S, B] = apply[S, B](xyModifier, xyModifier)
 
-  def apply(xModifier: Int => String, yModifier: Int => String): CliView = new CliView(xModifier, yModifier)
+  def apply[S <: BoardGameState[B], B <: RectangularBoardStructure]
+      (xModifier: Int => String, yModifier: Int => String): CliView[S, B] = new CliView[S, B](xModifier, yModifier)
 }
--- src/main/scala/sbags/interaction/view/View.scala
+++ src/main/scala/sbags/interaction/view/View.scala
@@ -3,14 +3,13 @@ package sbags.interaction.view
 import sbags.core.GameState
 import sbags.interaction.controller.InputListener
 
-trait View {
-  type State <: GameState
+trait View[S <: GameState] {
   def addListener(listener: InputListener): Unit
-  def moveAccepted(gameState: State): Unit
+  def moveAccepted(gameState: S): Unit
   def moveRejected(): Unit
 }
 
-abstract class BasicView extends View {
+abstract class BasicView[S <: GameState] extends View[S] {
   protected var listenerSet: Set[InputListener] = Set.empty
   override def addListener(listener: InputListener): Unit = listenerSet += listener
 }
