a15ae40d3b396027b4ce8b41c9efed89cea114b2
Simone <Simone.magnani4@studio.unibo.it>
2020-07-20 19:11:15 +0200

    feat: add Renderer to View

--- src/main/scala/sbags/interaction/view/CliView.scala
+++ src/main/scala/sbags/interaction/view/CliView.scala
@@ -1,19 +1,10 @@
 package sbags.interaction.view
 
-import sbags.core.BoardGameState._
 import sbags.core.{BoardGameState, RectangularBoardStructure}
 import sbags.interaction.controller.{Event, EventParser}
 
-class CliView[B <: RectangularBoardStructure, G](xModifier: Int => String,
-                                                 yModifier: Int => String,
-                                                 parser: EventParser)
-                                                (implicit ev: BoardGameState[B, G])
-
-  extends BasicView[G] {
-
-  private val stringifier = Stringifier[B](xModifier, yModifier)
-
-  override def moveAccepted(gameState: G): Unit = write(stringifier.buildBoard(gameState.boardState))
+class CliView[B <: RectangularBoardStructure, G](override val renderers: Seq[Renderer[G]], parser: EventParser)
+                                                (implicit ev: BoardGameState[B, G]) extends BasicView[G] {
 
   override def moveRejected(): Unit = write("last move was illegal")
 
@@ -29,13 +20,9 @@ class CliView[B <: RectangularBoardStructure, G](xModifier: Int => String,
     } yield()
 
   private def notify(event: Event): IO[_] = unit(listenerSet.foreach(_.notify(event)))
-
 }
 
 object CliView {
-  def apply[B <: RectangularBoardStructure, G](xyModifier: Int => String = _ + 1 + "")(parser: EventParser)(implicit ev: BoardGameState[B, G]): CliView[B, G] =
-    apply(xyModifier, xyModifier, parser)
-
-  def apply[B <: RectangularBoardStructure, G](xModifier: Int => String, yModifier: Int => String, parser: EventParser)(implicit ev: BoardGameState[B, G]): CliView[B, G] =
-    new CliView(xModifier, yModifier, parser)
+  def apply[B <: RectangularBoardStructure, G](renderers: Seq[Renderer[G]], parser: EventParser)(implicit ev: BoardGameState[B, G]): CliView[B, G] =
+    new CliView(renderers, parser)
 }
--- src/main/scala/sbags/interaction/view/View.scala
+++ src/main/scala/sbags/interaction/view/View.scala
@@ -3,8 +3,9 @@ package sbags.interaction.view
 import sbags.interaction.controller.InputListener
 
 trait View[G] {
+  val renderers: Seq[Renderer[G]]
   def addListener(listener: InputListener): Unit
-  def moveAccepted(gameState: G): Unit
+  def moveAccepted(gameState: G): Unit = renderers.foreach(_.render(gameState))
   def moveRejected(): Unit
   def nextCommand(): Unit
   def startGame(): Unit = nextCommand()
