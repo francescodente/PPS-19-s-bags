b5d37edbcdcfbfd1cf9011040102d2611ad5d9f1
Francesco Dente <dente.francesco1996@gmail.com>
2020-07-23 17:44:24 +0200

    feat: add "pawn is placed on tile" syntax

--- src/main/scala/sbags/core/dsl/Actions.scala
+++ src/main/scala/sbags/core/dsl/Actions.scala
@@ -1,8 +1,21 @@
 package sbags.core.dsl
 
+import sbags.core.{BoardGameState, BoardStructure}
+import sbags.core.BoardGameState._
+
 case class Action[G](run: G => G) {
   def >>(other: Action[G]): Action[G] = Action(g => other.run(run(g)))
 }
 
 trait Actions[G] {
+  implicit class IsOperations[P](pawn: P) {
+    def is[T](verb: P => T): T = verb(pawn)
+  }
+
+  def placed[B <: BoardStructure](implicit ev: BoardGameState[B, G]): B#Pawn => PlacedResult[B] =
+    PlacedResult(_)
+
+  case class PlacedResult[B <: BoardStructure](pawn: B#Pawn)(implicit ev: BoardGameState[B, G]) {
+    def on(tile: B#Tile): Action[G] = Action(g => g.setBoard(g.boardState place (pawn, tile)))
+  }
 }
--- src/test/scala/sbags/core/dsl/ActionsTest.scala
+++ src/test/scala/sbags/core/dsl/ActionsTest.scala
@@ -1,13 +1,37 @@
 package sbags.core.dsl
 
 import org.scalatest.{FlatSpec, Matchers}
+import sbags.core.{Board, BoardGameState, BoardStructure}
 
-class ActionsTest extends FlatSpec with Matchers {
+object TestBoard extends BoardStructure {
+  type Tile = Int
+  type Pawn = String
+
+  val pawn: Pawn = "a"
+  val tile: Tile = 0
+  override def tiles: Seq[Tile] = Seq(tile)
+}
+
+class ActionsTest extends FlatSpec with Matchers with Actions[Board[TestBoard.type]] {
   behavior of "An action"
 
   it can "be concatenated to another action" in {
     val a1 = Action[String](_ + "abc")
     val a2 = Action[String](_ + "def")
     (a1 >> a2).run("xyz") should be ("xyzabcdef")
+  }
+
+  implicit object TestBoardState extends BoardGameState[TestBoard.type, Board[TestBoard.type]] {
+    override def boardState(state: Board[TestBoard.type]): Board[TestBoard.type] = state
+
+    override def setBoard(state: Board[TestBoard.type])(board: Board[TestBoard.type]): Board[TestBoard.type] = board
+  }
+
+  behavior of "Board actions"
+
+  they can "be used to place pawns" in {
+    val state = Board(TestBoard)
+    val newState = (TestBoard.pawn is placed on TestBoard.tile).run(state)
+    newState(TestBoard.tile) should be (Some(TestBoard.pawn))
   }
 }
