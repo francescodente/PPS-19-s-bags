02d5f21b2624ac17527e78e943dbc99a293531e3
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-27 18:20:32 +0200

    feat: make companion object for BoardState with apply method

--- src/main/scala/sbags/core/extension/BoardState.scala
+++ src/main/scala/sbags/core/extension/BoardState.scala
@@ -6,3 +6,13 @@ trait BoardState[B <: BoardStructure, G] {
   def boardState(state: G): Board[B]
   def setBoard(state: G)(board: Board[B]): G
 }
+
+object BoardState {
+  def apply[B <: BoardStructure, G](stateToBoard: G => Board[B],
+                                    newState: (G, Board[B]) => G): BoardState[B, G] =
+    new BoardState[B, G] {
+      override def boardState(state: G): Board[B] = stateToBoard(state)
+
+      override def setBoard(state: G)(board: Board[B]): G = newState(state, board)
+    }
+}
