cb51fdafd0222d7fb60c03ade78c6c8e723ed591
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-07-27 18:27:39 +0200

    feature: add companion object for TurnState with apply

--- src/main/scala/sbags/core/extension/TurnState.scala
+++ src/main/scala/sbags/core/extension/TurnState.scala
@@ -4,3 +4,12 @@ trait TurnState[T, G] {
   def turn(state: G): T
   def nextTurn(state: G): G
 }
+
+object TurnState {
+  def apply[T, G](stateToTurn: G => T, toNextState: G => G): TurnState[T, G] =
+    new TurnState[T, G] {
+      override def turn(state: G): T = stateToTurn(state)
+
+      override def nextTurn(state: G): G = toNextState(state)
+    }
+}
