b544a8c7644273d8853a4851d9d7c8c727696c7b
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-08-08 09:33:19 +0200

    test: make MovesGenerationTest independent from Generators class and add multiple movesGeneration statement test

--- src/test/scala/sbags/core/dsl/MovesGenerationTest.scala
+++ src/test/scala/sbags/core/dsl/MovesGenerationTest.scala
@@ -3,79 +3,29 @@ package sbags.core.dsl
 import org.scalatest.{FlatSpec, Matchers}
 import sbags.core.dsl.Chainables._
 
-class MovesGenerationTest extends FlatSpec with Matchers {
+class MovesGenerationTest extends FlatSpec with Matchers with Generators[String, String]{
   private val state = "xyz"
-  private val moveA = "a"
-  private val moveB = "b"
-  private val moveC = "c"
-  private val allMoves = Seq(moveA, moveB, moveC)
-  private val chars = Feature[String, Seq[String]](_ map (_.toString))
 
   behavior of "A move generator"
 
   it should "generate no moves when created with no rules" in {
-    val gen = new MovesGeneration[String, String] with Generators[String, String] {
-      moveGeneration { nothing }
-    }
+    val gen = new MovesGeneration[String, String]{}
     gen.generateMoves(state) should be (empty)
   }
 
-  it should "generate moves explicitly" in {
-    val gen = new MovesGeneration[String, String] with Generators[String, String] {
-      moveGeneration {
-        generate (allMoves: _*)
-      }
+  it should "generate the moves specified in a single move generation statement" in {
+    val gen = new MovesGeneration[String, String] {
+      moveGeneration { s => s.toSeq.map("" + _) }
     }
-    gen.generateMoves(state) should contain theSameElementsAs allMoves
+    gen.generateMoves(state) should contain theSameElementsAs Seq("x","y","z")
   }
 
-  it should "generate moves when a condition is met" in {
-    val gen = new MovesGeneration[String, String] with Generators[String, String] with Modifiers[String] {
-      moveGeneration {
-        when (_ == state) {
-          generate (moveA)
-        }
-      }
+  it should "generate the union of moves defined in different move generation statements" in {
+    val gen = new MovesGeneration[String, String] {
+      moveGeneration { s => s.toSeq.take(2).map("" + _)}
+      moveGeneration { s => s.toSeq.drop(2).map("" + _)}
     }
-    gen.generateMoves(state) should contain theSameElementsAs Seq(moveA)
+    gen.generateMoves(state) should contain theSameElementsAs Seq("x","y","z")
   }
 
-  it should "not generate moves when a condition is not met" in {
-    val gen = new MovesGeneration[String, String] with Generators[String, String] with Modifiers[String] {
-      moveGeneration {
-        when (_ != state) {
-          generate (moveA)
-        }
-      }
-    }
-    gen.generateMoves(state) should be (empty)
-  }
-
-  it should "generate moves iteratively" in {
-    val gen = new MovesGeneration[String, String] with Generators[String, String] with Modifiers[String] {
-      moveGeneration {
-        iterating over chars as { c =>
-          generate (c)
-        }
-      }
-    }
-    gen.generateMoves(state) should contain theSameElementsAs (state map (_.toString))
-  }
-
-  it should "generate moves with nested rules" in {
-    val gen = new MovesGeneration[String, String] with Generators[String, String] with Modifiers[String] {
-      moveGeneration {
-        iterating over chars as { c1 =>
-          iterating over chars as { c2 =>
-            generate (c1 + c2)
-          }
-        }
-      }
-    }
-    val expected = for {
-      m1 <- state
-      m2 <- state
-    } yield m1.toString + m2.toString
-    gen.generateMoves(state) should contain theSameElementsAs expected
-  }
 }
