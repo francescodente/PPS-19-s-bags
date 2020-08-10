a67aaf652f27d0bafe5bb2b8db6202fded615361
Simone <Simone.magnani4@studio.unibo.it>
2020-08-10 14:53:02 +0200

    docs: update scaladoc

--- src/main/scala/sbags/interaction/GameSetup.scala
+++ src/main/scala/sbags/interaction/GameSetup.scala
@@ -2,8 +2,17 @@ package sbags.interaction
 
 import sbags.interaction.view.{Event, Renderer, RendererBuilder}
 
+/**
+ * Represents the entry point for each game that extends for it.
+ * @tparam M type of move playable in the game.
+ */
 trait GameSetup[M] extends App {
-  def eventsToMove(events: List[Event]): Option[M]
+  /**
+   * Defines how each Seq of events is represented as a move
+   * @param events the Seq of events
+   * @return Optional of move represented from the seq of events, None otherwise.
+   */
+  def eventsToMove(events: Seq[Event]): Option[M]
 }
 
 trait SetupRenderers[G, R <: Renderer[G]] {
