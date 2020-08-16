78b76c670b38c46bf4de6ecee4593184e6bc565b
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-08-16 15:52:49 +0200

    docs: scaladoc for controllers

--- src/main/scala/sbags/interaction/controller/ApplicationController.scala
+++ src/main/scala/sbags/interaction/controller/ApplicationController.scala
@@ -2,6 +2,13 @@ package sbags.interaction.controller
 
 import sbags.interaction.GameSetup
 
+/**
+ * The controller of the application.
+ * It sets up and starts the menu.
+ * @param gameSetup the [[sbags.interaction.GameSetup]] for the specific game the controller must handle.
+ * @tparam M type of move playable in the game.
+ * @tparam G type of the game state.
+ */
 class ApplicationController[M, G](gameSetup: GameSetup[M, G]) {
   def start(): Unit = {
     val menu = gameSetup.view.setupMenu()
--- src/main/scala/sbags/interaction/controller/MenuController.scala
+++ src/main/scala/sbags/interaction/controller/MenuController.scala
@@ -4,6 +4,13 @@ import sbags.interaction.GameSetup
 import sbags.interaction.view.{MenuViewListener, View}
 import sbags.model.core.GameDescription
 
+/**
+ * The controller of the menu phase.
+ * It supports handlers for quit and start game events.
+ * @param gameSetup the [[sbags.interaction.GameSetup]] for the specific game the controller must handle.
+ * @tparam M type of move playable in the game.
+ * @tparam G type of the game state.
+ */
 class MenuController[M, G](gameSetup: GameSetup[M, G]) extends MenuViewListener {
   override def onStartGame(): Unit = {
     val game = gameSetup.gameDescription.newGame
