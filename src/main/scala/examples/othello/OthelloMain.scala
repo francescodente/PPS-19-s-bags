32f753d72c6a97b839446bb463360eaf3bba30ea
Simone <Simone.magnani4@studio.unibo.it>
2020-08-17 17:17:39 +0200

    feat: add inputParser to othello and TicTacToe

--- src/main/scala/examples/othello/OthelloMain.scala
+++ src/main/scala/examples/othello/OthelloMain.scala
@@ -2,7 +2,7 @@ package examples.othello
 
 import examples.othello.Othello._
 import sbags.interaction.AppRunner
-import sbags.interaction.view.cli.{CliGameSetup, CliRenderer, Converter, Converters, RectangularBoardSetup}
+import sbags.interaction.view.cli.{CliGameSetup, CliRenderer, Converter, Converters, InputParserBuilder, RectangularBoardSetup}
 import sbags.interaction.view.{Event, RendererBuilder, TileSelected}
 import sbags.model.core.GameDescription
 
@@ -20,6 +20,9 @@ object OthelloSetup extends CliGameSetup[Move, State] with RectangularBoardSetup
 
   override def coordinateConverters: (Converter[Int], Converter[Int]) =
     (Converters.letters, Converters.oneBased)
+
+  override def setupInputParser(builder: InputParserBuilder): InputParserBuilder = builder
+    .addTileCommand()
 
   override def setupRenderers(rendering: RendererBuilder[State, CliRenderer[State]]): RendererBuilder[State, CliRenderer[State]] = rendering
     .withBoard
