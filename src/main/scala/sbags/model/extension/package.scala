bc8fb1c7d95ce5226362455398779945aa1673d4
Simone <Simone.magnani4@studio.unibo.it>
2020-08-17 18:56:48 +0200

    docs: add scaladoc to extension package obj

--- src/main/scala/sbags/model/extension/package.scala
+++ src/main/scala/sbags/model/extension/package.scala
@@ -2,26 +2,81 @@ package sbags.model
 
 import sbags.model.core.{Board, BoardStructure}
 
+/** Represents the object that needs to be import to use implicit extensions utilities. */
 package object extension {
+
+  /**
+   * Improve readability of code using the OO method call:
+   * state.method(implicit ev) instead method(state)(implicit ev).
+   * Adds method changeBoard.
+   *
+   * @param state the game state in which is called the method.
+   * @param ev the [[sbags.model.extension.BoardState]] on which are called methods.
+   * @tparam B type of BoardStructure.
+   * @tparam G type of game state.
+   */
   implicit class BoardGameStateOps[B <: BoardStructure, G](state: G)(implicit ev: BoardState[B, G]) {
     def boardState: Board[B] = ev.boardState(state)
     def setBoard(board: Board[B]): G = ev.setBoard(state)(board)
+
+    /**
+     * Returns the old state in which the board is changed with the modified one.
+     *
+     * @param modifier the updater from the old board to the new one.
+     * @return the old state with the new board.
+     */
     def changeBoard(modifier: Board[B] => Board[B]): G = state.setBoard(modifier(state.boardState))
   }
 
+  /**
+   * Improve readability of code using the OO method call:
+   * state.method(implicit ev) instead method(state)(implicit ev).
+   *
+   * @param state the game state in which is called the method.
+   * @param ev the [[sbags.model.extension.TurnState]] on which are called methods.
+   * @tparam T type of turn.
+   * @tparam G type of game state.
+   */
   implicit class TurnStateOps[T, G](state: G)(implicit ev: TurnState[T, G]) {
     def turn: T = ev.turn(state)
     def nextTurn(): G = ev.nextTurn(state)
   }
 
+  /**
+   * Improve readability of code using the OO method call:
+   * state.method(implicit ev) instead method(state)(implicit ev).
+   *
+   * @param state the game state in which is called the method.
+   * @param ev the [[sbags.model.extension.Players]] on which are called methods.
+   * @tparam P type of players.
+   * @tparam G type of game state.
+   */
   implicit class PlayersOps[P, G](state: G)(implicit ev: Players[P, G]) {
     def players: Seq[P] = ev.players(state)
   }
 
+  /**
+   * Improve readability of code using the OO method call:
+   * state.method(implicit ev) instead method(state)(implicit ev).
+   *
+   * @param state the game state in which is called the method.
+   * @param ev the [[sbags.model.extension.PlayersAsTurns]] on which are called methods.
+   * @tparam P type of players.
+   * @tparam G type of game state.
+   */
   implicit class PlayersAsTurnsOps[P, G](state: G)(implicit ev: PlayersAsTurns[P, G]) extends PlayersOps[P, G](state) {
     def currentPlayer: P = ev.turn(state)
   }
 
+  /**
+   * Improve readability of code using the OO method call:
+   * state.method(implicit ev) instead method(state)(implicit ev).
+   *
+   * @param state the game state in which is called the method.
+   * @param ev the [[sbags.model.extension.GameEndCondition]] on which are called methods.
+   * @tparam R type of result.
+   * @tparam G type of game state.
+   */
   implicit class GameEndConditionOps[R, G](state: G)(implicit ev: GameEndCondition[R, G]) {
     def gameResult: Option[R] = ev.gameResult(state)
   }
