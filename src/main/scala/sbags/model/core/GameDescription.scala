360d8033c9f60c409caa61e009bbb168e6dd1ea3
Simone <Simone.magnani4@studio.unibo.it>
2020-08-18 18:01:52 +0200

    docs: starting core scaladoc

--- src/main/scala/sbags/model/core/GameDescription.scala
+++ src/main/scala/sbags/model/core/GameDescription.scala
@@ -1,19 +1,31 @@
 package sbags.model.core
 
-/**
- * Represents the definition of a generic game, providing a factory to generate the initial state.
+/** Represents the definition of a generic game, providing a factory to generate the initial state.
+ *
+ * @tparam M the type of moves for this game.
+ * @tparam G the type of game state.
  */
 trait GameDescription[M, G] {
+  /** Represents the alias for the type of the moves. */
   type Move = M
+
+  /** Represents the alias for the type of the game state. */
   type State = G
 
   /**
    * Returns a new instance of the initial game state for this game description.
+   *
    * @return a new instance of the game state.
    */
   def newGame: Game[M, G] = Game(initialState, ruleSet)
 
+  /**
+   * Defines the initial game state used to create the game.
+   *
+   * @return the initial game state.
+   */
   protected def initialState: G
 
+  /** Defines the [[sbags.model.core.RuleSet]] used to create the game. */
   val ruleSet: RuleSet[M, G]
 }
--- src/main/scala/sbags/model/core/RuleSet.scala
+++ src/main/scala/sbags/model/core/RuleSet.scala
@@ -5,12 +5,12 @@ package sbags.model.core
  * for any given state of the game. The rule set can be used to obtain a collection
  * of all available moves for a given game state or to verify that a move is valid.
  *
- * @tparam M the type of moves for this game
- * @tparam G the type of game state
+ * @tparam M the type of moves for this game.
+ * @tparam G the type of game state.
  */
 trait RuleSet[M, G] {
   /**
-   * Returns true if the given move is valid for the implicitly defined game state.
+   * Returns true if the given move is valid for the given state.
    *
    * @param move the move to be checked.
    * @param state the state on which to check the move.
@@ -20,7 +20,7 @@ trait RuleSet[M, G] {
     availableMoves(state) contains move
 
   /**
-   * Returns a sequence of all possible moves for the implicitly defined game state.
+   * Returns a sequence of all possible moves for the given state.
    *
    * @param state the state on which to calculate moves.
    * @return the sequence of generated moves.
@@ -28,10 +28,11 @@ trait RuleSet[M, G] {
   def availableMoves(state: G): Seq[M]
 
   /**
-   * Executes a move on the actual GameState.
+   * Executes a move on the given state.
    *
    * @param move the move that needs to be executed.
    * @param state the state on which to execute move.
+   * @return the new state after the move.
    */
   def executeMove(move: M)(state: G): G
 }
