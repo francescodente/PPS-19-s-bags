f98437a3be2f149fd903b15c164b721e95ea75a4
Francesco Dente <dente.francesco1996@gmail.com>
2020-08-25 17:04:24 +0200

    test: extract method to fill the board in Othello tests

--- src/test/scala/examples/othello/OthelloTest.scala
+++ src/test/scala/examples/othello/OthelloTest.scala
@@ -2,11 +2,14 @@ package examples.othello
 
 import examples.othello.Othello._
 import org.scalatest.{FlatSpec, Matchers}
-import sbags.model.core.Board
-import sbags.model.extension.Results.Winner
+import sbags.model.core.{Board, Coordinate}
+import sbags.model.extension.Results.{Draw, Winner}
 import sbags.model.extension._
 
 class OthelloTest extends FlatSpec with Matchers {
+  def fillBoard(pawnSupplier: Coordinate => OthelloPawn): Board[BoardStructure] =
+    OthelloBoard.tiles.foldLeft(Board(OthelloBoard))((b, t) => b.place(pawnSupplier(t), t))
+
   behavior of "An Othello game"
 
   it should "start with the initial board configuration" in {
@@ -18,12 +21,15 @@ class OthelloTest extends FlatSpec with Matchers {
   }
 
   it should "end when the board is full" in {
-    val board = OthelloBoard.tiles.foldLeft(Board(OthelloBoard))((b, t) => b.place(Black, t))
+    val board = fillBoard(_ => Black)
     OthelloState(board, Black).gameResult shouldNot be (empty)
   }
 
-  it should "have the player with the highest number of pawns on the board as the winner" in {
-    val board = OthelloBoard.tiles.foldLeft(Board(OthelloBoard))((b, t) => b.place(Black, t))
+  it should "end with the player with the highest number of pawns on the board as the winner" in {
+    val board = fillBoard {
+      case t if t.x == 0 => White
+      case _ => Black
+    }
     OthelloState(board, Black).gameResult should be (Some(Winner(Black)))
   }
 }
