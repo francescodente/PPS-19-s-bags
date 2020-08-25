cb10531b3ad77776056fe1a5a7ebe8f74f25da97
Francesco Dente <dente.francesco1996@gmail.com>
2020-08-25 17:18:20 +0200

    test: add test for game end with non-full board

--- src/test/scala/examples/othello/OthelloTest.scala
+++ src/test/scala/examples/othello/OthelloTest.scala
@@ -7,8 +7,20 @@ import sbags.model.extension.Results.{Draw, Winner}
 import sbags.model.extension._
 
 class OthelloTest extends FlatSpec with Matchers {
-  def fillBoard(pawnSupplier: Coordinate => OthelloPawn): Board[BoardStructure] =
+  private def fillBoard(pawnSupplier: Coordinate => OthelloPawn): Board[BoardStructure] =
     OthelloBoard.tiles.foldLeft(Board(OthelloBoard))((b, t) => b.place(pawnSupplier(t), t))
+
+  private val withCheckerBoardPattern: Coordinate => OthelloPawn = {
+    case t if (t.x + t.y) % 2 == 0 => White
+    case _ => Black
+  }
+
+  private val withOneWhiteLine: Coordinate => OthelloPawn =  {
+    case t if t.x == 0 => White
+    case _ => Black
+  }
+
+  private val withOnly: OthelloPawn => Coordinate => OthelloPawn = p => _ => p
 
   behavior of "An Othello game"
 
@@ -21,23 +33,22 @@ class OthelloTest extends FlatSpec with Matchers {
   }
 
   it should "end when the board is full" in {
-    val board = fillBoard(_ => Black)
+    val board = fillBoard(withOnly(Black))
     OthelloState(board, Black).gameResult shouldNot be (empty)
   }
 
+  it should "end when no moves are available for both players" in {
+    val board = fillBoard(withOnly(Black)).clear(0, 0)
+    OthelloState(board, White).gameResult shouldNot be (empty)
+  }
+
   it should "end with the player with the highest number of pawns on the board as the winner" in {
-    val board = fillBoard {
-      case t if t.x == 0 => White
-      case _ => Black
-    }
+    val board = fillBoard(withOneWhiteLine)
     OthelloState(board, Black).gameResult should be (Some(Winner(Black)))
   }
 
   it should "end in a draw when pawns are in equal number" in {
-    val board = fillBoard {
-      case t if (t.x + t.y) % 2 == 0 => White
-      case _ => Black
-    }
+    val board = fillBoard(withCheckerBoardPattern)
     OthelloState(board, Black).gameResult should be (Some(Draw))
   }
 }
