ec48d9c10bce8e8aa90c3be5d090d4a2f6792e22
Francesco Dente <dente.francesco1996@gmail.com>
2020-08-25 18:47:55 +0200

    test: refactor Game tests

--- src/test/scala/sbags/model/core/GameTest.scala
+++ src/test/scala/sbags/model/core/GameTest.scala
@@ -4,12 +4,12 @@ import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 
 class GameTest extends FlatSpec with MockFactory with Matchers {
-  type MoveMock = Int => Int
+  type MoveMock = Int
   type StateMock = Int
 
   private val ruleSetMock = mock[RuleSet[MoveMock, StateMock]]
   private val initState: StateMock = 0
-  private def defaultMove: MoveMock = _ + 1
+  private val defaultMove: MoveMock = 1
 
   private def newGameTest: Game[MoveMock, StateMock] = Game(initState, ruleSetMock)
 
@@ -25,22 +25,21 @@ class GameTest extends FlatSpec with MockFactory with Matchers {
 
   it should "have the new state if tries a valid move" in {
     val newState = 1
-    (ruleSetMock.isValid(_:MoveMock)(_:StateMock)).expects(*,*).returns(true).once()
-    (ruleSetMock.executeMove(_:MoveMock)(_:StateMock)).expects(*,*).returns(newState).once()
+    (ruleSetMock.isValid(_:MoveMock)(_:StateMock)).expects(defaultMove, initState).returns(true)
+    (ruleSetMock.executeMove(_: MoveMock)(_: StateMock)).expects(defaultMove, initState).returns(newState)
     val game = newGameTest
-    game executeMove defaultMove
-    game.currentState should be (newState)
+    game executeMove defaultMove should be (Right(newState))
   }
 
   it should "fail if it tries an invalid Move" in {
-    (ruleSetMock.isValid(_:MoveMock)(_:StateMock)).expects(*,*).returns(false).once()
+    (ruleSetMock.isValid(_:MoveMock)(_:StateMock)).expects(defaultMove, initState).returns(false)
     newGameTest.executeMove(defaultMove) should be (Left(InvalidMove))
   }
 
   it should "fail if it tries a valid Move but something goes wrong in execution" in {
     val exception = new IllegalStateException
-    (ruleSetMock.isValid(_:MoveMock)(_:StateMock)).expects(*,*).returns(true).once()
-    (ruleSetMock.executeMove(_:MoveMock)(_:StateMock)).expects(*,*).throws(exception).once()
+    (ruleSetMock.isValid(_:MoveMock)(_:StateMock)).expects(defaultMove, initState).returns(true)
+    (ruleSetMock.executeMove(_:MoveMock)(_:StateMock)).expects(defaultMove, initState).throws(exception)
     newGameTest.executeMove(defaultMove) should be (Left(Error(exception)))
   }
 }
