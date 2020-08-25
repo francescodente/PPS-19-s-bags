c281d117ee865eb54ec412a4caa2ee71a3bbf96c
Shapour Nemati <shapour.nemati@studio.unibo.it>
2020-08-25 17:21:29 +0200

    docs: tic tac toe comments

--- src/main/scala/examples/tictactoe/TicTacToeMain.scala
+++ src/main/scala/examples/tictactoe/TicTacToeMain.scala
@@ -13,14 +13,17 @@ object TicTacToeMain extends App {
 object TicTacToeSetup extends CliGameSetup[Move, State] with RectangularBoardSetup[BoardStructure, State] {
   override val gameDescription: GameDescription[Move, State] = TicTacToe
 
+  /** Enable the renderers for the used extensions. */
   override def setupRenderers(rendering: RendererBuilder[State, CliRenderer[State]]): RendererBuilder[State, CliRenderer[State]] = rendering
     .withBoard
     .withTurns
     .withGameResult
 
+  /** In TicTacToe we just select a tile to make a move, so no other command is required. */
   override def setupInputParser(builder: InputParserBuilder): InputParserBuilder = builder
     .addTileCommand()
 
+  /** When a tile is selected we want the Put move to be recognized, otherwise no move should be found. */
   override def eventsToMove(events: List[Event]): Option[Move] = events match {
     case TileSelected(x, y) :: Nil => Some(Put(x, y))
     case _ => None
